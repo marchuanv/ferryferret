94249a2431ea404158dac2ce0c4fb8f9c2b0de48
marchuanv <Marchuanv@gmail.com>
2018-08-15 12:02:07 +0200

    Update github.js

--- github.js
+++ github.js
@@ -1,1 +1,10 @@
-const git = require('github-client');
+const git=require('github-client');
+const github;
+module.exports={
+  login:(_password) => {
+    github=git.new({
+      username:'marchuanv',
+      password: _password
+    });
+  }
+};
