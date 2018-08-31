9d4e8f390bcd8aa92042630bce974b00f107f77f
marchuanv <Marchuanv@gmail.com>
2018-08-31 10:18:43 +0200

    Update and rename bootstrap.js to bootstrap.client.js

--- bootstrap.client.js
+++ bootstrap.client.js
@@ -34,28 +34,25 @@ function Bootstrap(){
 	};
 
 	this.load=async()=>{
-		if (typeof window !== 'undefined'){
-			await request("/package.json").then(async(pkgJson)=>{
-				const pkg = JSON.parse(pkgJson);
-				await request("/config.json").then(async(configJson)=>{
-					const config = JSON.parse(configJson);
-					const endpoint = config.find((e)=> e.gitname==pkg.name);
-					const promises=[];
-					await getAllEndpointDependencies(config, endpoint).then((endpoints)=>{
-						for(const endpoint of endpoints){
-							const promise=new Promise((resolve)=>{
-								const script = document.createElement('script');
-								script.src = endpoint.url;
-								script.onload=resolve;
-								document.head.appendChild(script);
-							});
-							promises.push(promise);
-						};
-					});
-					return Promise.all(promises);
+		await request("/package.json").then(async(pkgJson)=>{
+			const pkg = JSON.parse(pkgJson);
+			await request("/config.json").then(async(configJson)=>{
+				const config = JSON.parse(configJson);
+				const endpoint = config.find((e)=> e.gitname==pkg.name);
+				const promises=[];
+				await getAllEndpointDependencies(config, endpoint).then((endpoints)=>{
+					for(const endpoint of endpoints){
+						const promise=new Promise((resolve)=>{
+							const script = document.createElement('script');
+							script.src = endpoint.url;
+							script.onload=resolve;
+							document.head.appendChild(script);
+						});
+						promises.push(promise);
+					};
 				});
-
+				return Promise.all(promises);
 			});
-		}
+		});
 	};
 }
